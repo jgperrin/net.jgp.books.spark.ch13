09d803642dce543580396b5337b9daa47bd243e4
jgp <jgp@10.0.100.31>
2019-01-19 11:30:39 -0500

    Formatting

--- src/main/java/net/jgp/books/sparkInAction/ch12/lab100RecordTransformation/RecordTransformationApp.java
+++ src/main/java/net/jgp/books/sparkInAction/ch12/lab100RecordTransformation/RecordTransformationApp.java
@@ -29,16 +29,17 @@ public class RecordTransformationApp {
    */
   private void start() {
 
-    SparkSession spark = SparkSession.builder()
-        .appName("Record transformations")
-        .master("local")
-        .getOrCreate();
+    SparkSession spark = SparkSession.builder().appName(
+        "Record transformations").master("local").getOrCreate();
 
-    Dataset<Row> countyStateDf = spark.read().format("csv")
+    Dataset<Row> intermediateDf = spark
+        .read()
+        .format("csv")
         .option("header", "true")
         .option("inferSchema", "true")
         .load("data/census/PEP_2017_PEPANNRES.csv");
-    countyStateDf = countyStateDf.drop("GEO.id")
+    intermediateDf = intermediateDf
+        .drop("GEO.id")
         .withColumnRenamed("GEO.id2", "id")
         .withColumnRenamed("GEO.display-label", "label")
         .withColumnRenamed("rescen42010", "real2010")
@@ -51,51 +52,42 @@ public class RecordTransformationApp {
         .withColumnRenamed("respop72015", "est2015")
         .withColumnRenamed("respop72016", "est2016")
         .withColumnRenamed("respop72017", "est2017");
-    countyStateDf.printSchema();
-    countyStateDf.show(5);
+    intermediateDf.printSchema();
+    intermediateDf.show(5);
 
     // Creates the additional columns
-    countyStateDf = countyStateDf
-        .withColumn(
-            "countyState",
-            split(countyStateDf.col("label"), ", "))
+    intermediateDf = intermediateDf
+        .withColumn("countyState", split(intermediateDf.col("label"), ", "))
         .withColumn("stateId", expr("int(id/1000)"))
         .withColumn("countyId", expr("id%1000"));
-    countyStateDf.printSchema();
-    countyStateDf.sample(.01).show(5, false);
+    intermediateDf.printSchema();
+    intermediateDf.sample(.01).show(5, false);
 
-    countyStateDf = countyStateDf
-        .withColumn("state", countyStateDf.col("countyState").getItem(1))
-        .withColumn("county", countyStateDf.col("countyState").getItem(0))
+    intermediateDf = intermediateDf.withColumn("state", intermediateDf.col(
+        "countyState").getItem(1))
+        .withColumn("county", intermediateDf.col("countyState").getItem(0))
         .drop("countyState");
-    countyStateDf.printSchema();
-    countyStateDf.sample(.01).show(5, false);
+    intermediateDf.printSchema();
+    intermediateDf.sample(.01).show(5, false);
 
     // I could split the column in one operation if I wanted:
     // @formatter:off
-    //    Dataset<Row> countyStateDf = countyStateDf
+    //    Dataset<Row> countyStateDf = intermediateDf
     //        .withColumn(
     //            "state",
-    //            split(countyStateDf.col("label"), ", ").getItem(1))
+    //            split(intermediateDf.col("label"), ", ").getItem(1))
     //        .withColumn(
     //            "county",
-    //            split(countyStateDf.col("label"), ", ").getItem(0));
+    //            split(intermediateDf.col("label"), ", ").getItem(0));
     // @formatter:on
-
     // Performs some statistics on the
-    Dataset<Row> statDf = countyStateDf
-        .withColumn("diff", expr("est2010-real2010"))
-        .withColumn("growth", expr("est2017-est2010"))
-        .drop("id")
-        .drop("label")
-        .drop("real2010")
-        .drop("est2010")
-        .drop("est2011")
-        .drop("est2012")
-        .drop("est2013")
-        .drop("est2014")
-        .drop("est2015")
-        .drop("est2016")
+    Dataset<Row> statDf = intermediateDf.withColumn("diff", expr(
+        "est2010-real2010"))
+        .withColumn("growth", expr("est2017-est2010")).drop("id").drop(
+            "label")
+        .drop("real2010").drop("est2010")
+        .drop("est2011").drop("est2012").drop("est2013").drop("est2014")
+        .drop("est2015").drop("est2016")
         .drop("est2017");
     statDf.printSchema();
     statDf.sample(.01).show(5, false);
