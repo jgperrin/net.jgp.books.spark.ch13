6e8cc2eff1e2d80f993f8ba350698c475d017411
jgp <jgp@10.0.100.124>
2019-01-20 20:29:15 -0500

    reformart

--- src/main/java/net/jgp/books/sparkInAction/ch12/lab100RecordTransformation/RecordTransformationApp.java
+++ src/main/java/net/jgp/books/sparkInAction/ch12/lab100RecordTransformation/RecordTransformationApp.java
@@ -28,16 +28,21 @@ public class RecordTransformationApp {
    * The processing code.
    */
   private void start() {
+    // Creation of the session
+    SparkSession spark = SparkSession.builder()
+        .appName("Record transformations")
+        .master("local")
+        .getOrCreate();
 
-    SparkSession spark = SparkSession.builder().appName(
-        "Record transformations").master("local").getOrCreate();
-
+    // Ingestion of the census data
     Dataset<Row> intermediateDf = spark
         .read()
         .format("csv")
         .option("header", "true")
         .option("inferSchema", "true")
         .load("data/census/PEP_2017_PEPANNRES.csv");
+
+    // Renaming and dropping the columns we do not need
     intermediateDf = intermediateDf
         .drop("GEO.id")
         .withColumnRenamed("GEO.id2", "id")
@@ -57,15 +62,21 @@ public class RecordTransformationApp {
 
     // Creates the additional columns
     intermediateDf = intermediateDf
-        .withColumn("countyState", split(intermediateDf.col("label"), ", "))
+        .withColumn(
+            "countyState",
+            split(intermediateDf.col("label"), ", "))
         .withColumn("stateId", expr("int(id/1000)"))
         .withColumn("countyId", expr("id%1000"));
     intermediateDf.printSchema();
     intermediateDf.sample(.01).show(5, false);
 
-    intermediateDf = intermediateDf.withColumn("state", intermediateDf.col(
-        "countyState").getItem(1))
-        .withColumn("county", intermediateDf.col("countyState").getItem(0))
+    intermediateDf = intermediateDf
+        .withColumn(
+            "state",
+            intermediateDf.col("countyState").getItem(1))
+        .withColumn(
+            "county",
+            intermediateDf.col("countyState").getItem(0))
         .drop("countyState");
     intermediateDf.printSchema();
     intermediateDf.sample(.01).show(5, false);
@@ -81,15 +92,30 @@ public class RecordTransformationApp {
     //            split(intermediateDf.col("label"), ", ").getItem(0));
     // @formatter:on
     // Performs some statistics on the
-    Dataset<Row> statDf = intermediateDf.withColumn("diff", expr(
-        "est2010-real2010"))
-        .withColumn("growth", expr("est2017-est2010")).drop("id").drop(
-            "label")
-        .drop("real2010").drop("est2010")
-        .drop("est2011").drop("est2012").drop("est2013").drop("est2014")
-        .drop("est2015").drop("est2016")
+    Dataset<Row> statDf = intermediateDf
+        .withColumn("diff", expr("est2010-real2010"))
+        .withColumn("growth", expr("est2017-est2010"))
+        .drop("id")
+        .drop("label")
+        .drop("real2010")
+        .drop("est2010")
+        .drop("est2011")
+        .drop("est2012")
+        .drop("est2013")
+        .drop("est2014")
+        .drop("est2015")
+        .drop("est2016")
         .drop("est2017");
     statDf.printSchema();
     statDf.sample(.01).show(5, false);
+
+    // Extras
+    statDf = statDf.sort(statDf.col("growth").desc());
+    System.out.println("Top 5 counties with the most growth:");
+    statDf.show(5, false);
+
+    statDf = statDf.sort(statDf.col("growth"));
+    System.out.println("Top 5 counties with the most loss:");
+    statDf.show(5, false);
   }
 }
