209823a317cf8feac53623414e10cd550e82f557
Jean Georges Perrin <jgp@jgp.net>
2019-02-26 21:36:51 -0500

    Making chapter 13 ready

--- src/main/java/net/jgp/books/sparkInAction/ch13/lab100_xx/RecordTransformationApp.java
+++ src/main/java/net/jgp/books/sparkInAction/ch13/lab100_xx/RecordTransformationApp.java
@@ -1,4 +1,4 @@
-package net.jgp.books.sparkInAction.ch12.lab200RecordTransformation;
+package net.jgp.books.sparkInAction.ch13.lab100_xx;
 
 import static org.apache.spark.sql.functions.expr;
 import static org.apache.spark.sql.functions.split;
