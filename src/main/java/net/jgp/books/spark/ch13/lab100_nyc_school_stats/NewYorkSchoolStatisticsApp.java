d872d59b8b53eb4886f1b368af34c313c237d762
Jean Georges Perrin <jgp@jgp.net>
2019-03-04 08:39:20 -0500

    Cleanup

--- src/main/java/net/jgp/books/spark/ch13/lab100_nyc_school_stats/NewYorkSchoolStatisticsApp.java
+++ src/main/java/net/jgp/books/spark/ch13/lab100_nyc_school_stats/NewYorkSchoolStatisticsApp.java
@@ -1,21 +1,26 @@
-package net.jgp.books.spark.ch13.lab100_xx;
+package net.jgp.books.spark.ch13.lab100_nyc_school_stats;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.length;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * CSV ingestion in a dataframe with a Schema.
+ * NYC schools analytics.
  * 
  * @author jgp
  */
 public class NewYorkSchoolStatisticsApp {
   private static Logger log = LoggerFactory
       .getLogger(NewYorkSchoolStatisticsApp.class);
-  
 
   /**
    * main() is your entry point to the application.
@@ -34,7 +39,7 @@ public class NewYorkSchoolStatisticsApp {
   private void start() {
     // Creates a session on a local master
     SparkSession spark = SparkSession.builder()
-        .appName("Complex CSV with a schema to Dataframe")
+        .appName("NYC schools analytics")
         .master("local[*]")
         .getOrCreate();
 
@@ -55,36 +60,53 @@ public class NewYorkSchoolStatisticsApp {
         DataTypes.createStructField(
             "enrolled",
             DataTypes.IntegerType,
-            false), 
+            false),
         DataTypes.createStructField(
             "present",
             DataTypes.IntegerType,
-            false), 
+            false),
         DataTypes.createStructField(
             "absent",
             DataTypes.IntegerType,
-            false), 
+            false),
         DataTypes.createStructField(
             "released",
             DataTypes.IntegerType,
             false) });
 
-    // Reads a CSV file with header, called books.csv, stores it in a dataframe
+    // Reads a CSV file with header, called books.csv, stores it in a
+    // dataframe
     Dataset<Row> df = spark.read().format("csv")
         .option("header", "true")
         .option("dateFormat", "yyyyMMdd")
         .schema(schema)
         .load("data/nyc_school_attendance/20*.csv");
+    log.debug("Datasets contains {} rows", df.count());
+
+    // FIltering, data preparation
+    df = df.filter(length(col("schoolYear")).$greater$eq(8));
+    df = df
+        .withColumn(
+            "year",
+            col("schoolYear").substr(0, 4).cast(DataTypes.IntegerType))
+        .drop("schoolYear");
 
     // Shows at most 5 rows from the dataframe
     df.show(5);
     df.printSchema();
     log.debug("Datasets contains {} rows", df.count());
-    
+
     // Unique schools
     Dataset<Row> uniqueSchoolsDf = df.select("schoolId").distinct();
     uniqueSchoolsDf.show(5);
     uniqueSchoolsDf.printSchema();
     log.debug("Datasets contains {} rows", uniqueSchoolsDf.count());
+
+    // Calculating the average enrollment for each school
+    Dataset<Row> averageEnrollmentDf = df
+        .groupBy(col("schoolId"), col("year"))
+        .avg("enrolled", "present", "absent")
+        .orderBy("schoolId", "year");
+    averageEnrollmentDf.show(20);
   }
 }
