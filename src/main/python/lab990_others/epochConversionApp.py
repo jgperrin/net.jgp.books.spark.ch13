2b94648d278b6c4700c7869403efc80519662778
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2020-06-17 23:36:36 +0100

    Scala and Python Porting code

--- src/main/python/lab990_others/epochConversionApp.py
+++ src/main/python/lab990_others/epochConversionApp.py
@@ -1,43 +1,52 @@
 """
- Use of from_unixtime().
+  Use of from_unixtime().
 
  @author rambabu.posa
 """
 import time
 import random
+import logging
 from pyspark.sql import (SparkSession, functions as F)
 from pyspark.sql.types import (StructType, StructField,
                                StringType, IntegerType)
 
-# Creates a session on a local master
-spark = SparkSession.builder.appName("from_unixtime function") \
-    .master("local[*]").getOrCreate()
+def main(spark):
+    schema = StructType([
+        StructField('event', IntegerType(), False),
+        StructField('ts', StringType(), False)
+    ])
 
-schema = StructType([
-    StructField('event', IntegerType(), False),
-    StructField('ts', StringType(), False)
-])
+    now = int(time.time())
 
-now = int(time.time())
+    data = []
 
-data = []
+    # Building a df with a sequence of chronological timestamps
+    for i in range(0,1000):
+        data = data + [(i, now)]
+        now = now + (random.randint(1,3) + 1)
 
-# Building a df with a sequence of chronological timestamps
-for i in range(0,1000):
-    data = data + [(i, now)]
-    now = now + (random.randint(1,3) + 1)
+    df = spark.createDataFrame(data, schema)
+    df.show()
 
-df = spark.createDataFrame(data, schema)
-df.show()
+    # Turning the timestamps to dates
+    df = df.withColumn("date", F.from_unixtime(F.col("ts")))
+    df.show()
 
-# Turning the timestamps to dates
-df = df.withColumn("date", F.from_unixtime(F.col("ts")))
-df.show()
+    # Collecting the result and printing out
+    timeRows = [row for row in df.collect()]
 
-# Collecting the result and printing out
-timeRows = [row for row in df.collect()]
+    for row in timeRows:
+        logging.warning(f"{row[0]} : {row[1]} ({row[2]})")
 
-for row in timeRows:
-    print("{} : {} ({})".format(row[0], row[1], row[2]))
 
-spark.stop()
+if __name__ == "__main__":
+    # Creates a session on a local master
+    spark = SparkSession.builder.appName("from_unixtime()") \
+        .master("local[*]").getOrCreate()
+
+    # setting log level, update this as per your requirement
+    spark.sparkContext.setLogLevel("warn")
+
+    main(spark)
+    spark.stop()
+
--- src/main/python/lab990_others/epochTimestampConversionApp.py
+++ src/main/python/lab990_others/epochTimestampConversionApp.py
@@ -5,49 +5,55 @@
 """
 import time
 import random
+import logging
 from pyspark.sql import (SparkSession, functions as F)
 from pyspark.sql.types import (StructType, StructField,
                                StringType, IntegerType)
 
-# Creates a session on a local master
-spark = SparkSession.builder.appName('Timestamp conversion') \
-    .master('local[*]').getOrCreate()
 
-#spark.conf.set("spark.sql.session.timeZone", "GMT")
+def main(spark):
+    schema = StructType([
+        StructField('event', IntegerType(), False),
+        StructField('original_ts', StringType(), False)
+    ])
 
-schema = StructType([
-    StructField('event', IntegerType(), False),
-    StructField('original_ts', StringType(), False)
-])
+    now = int(time.time())
 
-now = int(time.time())
+    data = []
 
-data = []
+    # Building a df with a sequence of chronological timestamps
+    for i in range(0,1000):
+        data = data + [(i, now)]
+        now = now + (random.randint(1,3) + 1)
 
-# Building a df with a sequence of chronological timestamps
-for i in range(0,1000):
-    data = data + [(i, now)]
-    now = now + (random.randint(1,3) + 1)
+    df = spark.createDataFrame(data, schema)
+    df.show()
+    df.printSchema()
 
-df = spark.createDataFrame(data, schema)
-df.show()
-df.printSchema()
+    # Turning the timestamps to Timestamp datatype
+    # timestamp, format='yyyy-MM-dd HH:mm:ss')
+    df = df.withColumn('date', F.from_unixtime(df.original_ts).cast('timestamp'))
+    df.show(truncate=False)
+    df.printSchema()
 
-# Turning the timestamps to Timestamp datatype
-# timestamp, format='yyyy-MM-dd HH:mm:ss')
-df = df.withColumn('date', F.from_unixtime(df.original_ts).cast('timestamp'))
-df.show(truncate=False)
-df.printSchema()
+    # Turning back the timestamps to epoch
+    df = df.withColumn('epoch', F.unix_timestamp(df.date))
+    df.show(truncate=False)
+    df.printSchema()
 
-# Turning back the timestamps to epoch
-df = df.withColumn('epoch', F.unix_timestamp(df.date))
-df.show(truncate=False)
-df.printSchema()
+    # Collecting the result and printing out
+    timeRows = [row for row in df.collect()]
 
-# Collecting the result and printing out
-timeRows = [row for row in df.collect()]
+    for row in timeRows:
+        logging.warning("{} : {} ({})".format(row[0], row[1], row[2]))
 
-for row in timeRows:
-    print("{} : {} ({})".format(row[0], row[1], row[2]))
+if __name__ == "__main__":
+    # Creates a session on a local master
+    spark = SparkSession.builder.appName("from_unixtime()") \
+        .master("local[*]").getOrCreate()
 
-spark.stop()
+    # setting log level, update this as per your requirement
+    spark.sparkContext.setLogLevel("warn")
+
+    main(spark)
+    spark.stop()
